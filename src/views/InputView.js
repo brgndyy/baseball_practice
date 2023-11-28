a4257cd206824f557ea91f04be3dd28c0d40954d
brgndy <brgndy96@gmail.com>
2023-11-28 14:46:32 +0900

    feat(play): 공 입력 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,13 @@
-const InputView = {};
+import { Console } from '@woowacourse/mission-utils';
+
+const InputView = {
+  /**
+   * 시작 메세지를 읽음
+   * @param { string } message
+   */
+  async inputBalls(message) {
+    return await Console.readLineAsync(message);
+  },
+};
 
 export default InputView;
