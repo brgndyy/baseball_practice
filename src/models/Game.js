1b3459348899905a5b8ba4c2fa4df744b8aeebc1
brgndy <brgndy96@gmail.com>
2023-11-28 16:20:57 +0900

    feat(game): 게임 진행 클래스 코드 생성

--- src/models/Game.js
+++ src/models/Game.js
@@ -1,11 +1,60 @@
 import Opponent from './Opponnet.js';
 import Ball from './Ball.js';
+import GAME_CONDITION from '../constants/conditions/gameCondition.js';
 
 class Game {
   #opponent;
 
+  #opponentGongs;
+
+  #userBalls;
+
   constructor() {
     this.#opponent = new Opponent();
+  }
+
+  compareBalls(balls) {
+    const receivedBalls = new Ball(balls);
+
+    this.#userBalls = receivedBalls.getBalls();
+    this.#opponentGongs = this.#opponent.getBalls();
+
+    const strikeCounts = this.#getStrikeCounts();
+    const ballCounts = this.#getBallCounts();
+
+    const gameState = {
+      ...(strikeCounts > 0 && { strikeCounts }),
+      ...(ballCounts > 0 && { ballCounts }),
+    };
+
+    const isSuccess = strikeCounts === GAME_CONDITION.success_condition;
+
+    return {
+      success: isSuccess,
+      gameState,
+    };
+  }
+
+  #getStrikeCounts() {
+    let strikes = 0;
+
+    for (let i = 0; i < this.#opponentGongs.length; i++) {
+      if (this.#opponentGongs[i] === this.#userBalls[i]) {
+        strikes++;
+      }
+    }
+
+    return strikes;
+  }
+
+  #getBallCounts() {
+    const commonBalls = this.#userBalls.filter((ball) => this.#opponentGongs.includes(ball)).length;
+
+    const strikes = this.#userBalls.filter(
+      (ball, index) => ball === this.#opponentGongs[index],
+    ).length;
+
+    return commonBalls - strikes;
   }
 }
 
