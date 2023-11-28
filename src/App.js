84a2eb7713eef6fb1bad9e6cc6c55994a1c71f18
brgndy <brgndy96@gmail.com>
2023-11-28 16:28:10 +0900

    feat(game): 정답 맞춘후 다시 시작 or 끝  구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import OutputView from './views/OutputView.js';
 import InputView from './views/InputView.js';
+import GAME_CONDITION from './constants/conditions/gameCondition.js';
+import Game from './models/Game.js';
 class App {
+  #game;
+
   async play() {
     this.#printStartMessage();
+    await this.#playGame();
   }
 
   #printStartMessage() {
     OutputView.printStartMessage();
+  }
+
+  async #playGame() {
+    this.#createGame();
+    await this.#inputBalls();
+  }
+
+  #createGame() {
+    this.#game = new Game();
+  }
+
+  async #inputBalls() {
+    while (true) {
+      const balls = await InputView.inputBalls();
+
+      const { success, gameState } = this.#game.compareBalls(balls);
+
+      if (success) {
+        OutputView.printGameResult(gameState);
+        OutputView.printSuccessMessage();
+        break;
+      }
+
+      OutputView.printGameResult(gameState);
+    }
+
+    await this.#chooseRestartOrEnd();
+  }
+
+  async #chooseRestartOrEnd() {
+    const inputEndOrRestart = await InputView.selectRestartOrEnd();
+
+    if (inputEndOrRestart === GAME_CONDITION.restart_game) {
+      await this.#playGame();
+    }
   }
 }
 
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,12 +1,27 @@
 import { Console } from '@woowacourse/mission-utils';
+import PROGRESS_MESSAGES from '../constants/messages/progressMessages.js';
+import AppError from '../error/AppError.js';
+import GAME_CONDITION from '../constants/conditions/gameCondition.js';
+import ERROR_MESSAGES from '../constants/messages/errorMessages.js';
 
 const InputView = {
   /**
    * 시작 메세지를 읽음
    * @param { string } message
    */
-  async inputBalls(message) {
-    return await Console.readLineAsync(message);
+  async inputBalls() {
+    const balls = await Console.readLineAsync(PROGRESS_MESSAGES.input_balls);
+    return balls.split('').map((ball) => parseInt(ball, 10));
+  },
+
+  async selectRestartOrEnd() {
+    const state = await Console.readLineAsync(PROGRESS_MESSAGES.select_restart_or_end);
+
+    if (state !== GAME_CONDITION.restart_game && state !== GAME_CONDITION.end_game) {
+      throw new AppError(ERROR_MESSAGES.not_match_end_or_restart);
+    }
+
+    return state;
   },
 };
 
