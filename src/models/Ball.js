13ec8dedf0c1da53c9469c317a269742875e5d0c
brgndy <brgndy96@gmail.com>
2023-11-28 16:19:25 +0900

    feat(ball): 게임 공을 담당할 Ball 클래스 선언

--- src/models/Ball.js
+++ src/models/Ball.js
@@ -1,7 +1,54 @@
-import { Random } from '@woowacourse/mission-utils';
+import ERROR_MESSAGES from '../constants/messages/errorMessages.js';
+import AppError from '../error/AppError.js';
 
 class Ball {
-  constructor() {}
+  static MAX_LENGTH = 3;
+
+  static MIN_NUM = 1;
+
+  static MAX_NUM = 9;
+
+  #balls;
+
+  constructor(balls) {
+    this.#balls = balls;
+    this.#validate();
+  }
+
+  #validate() {
+    this.#validateType();
+    this.#validateLength();
+    this.#validateRange();
+    this.#validateDupliacation();
+  }
+
+  #validateType() {
+    if (this.#balls.some(Number.isNaN)) {
+      throw new AppError(ERROR_MESSAGES.not_number);
+    }
+  }
+
+  #validateLength() {
+    if (this.#balls.length !== Ball.MAX_LENGTH) {
+      throw new AppError(ERROR_MESSAGES.not_match_length);
+    }
+  }
+
+  #validateRange() {
+    if (!this.#balls.every((num) => num >= Ball.MIN_NUM && num <= Ball.MAX_NUM)) {
+      throw new AppError(ERROR_MESSAGES.out_of_range);
+    }
+  }
+
+  #validateDupliacation() {
+    if (this.#balls.length !== new Set(this.#balls).size) {
+      throw new AppError(ERROR_MESSAGES.have_duplication);
+    }
+  }
+
+  getBalls() {
+    return this.#balls;
+  }
 }
 
 export default Ball;
